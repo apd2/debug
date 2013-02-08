fefb85c49d4b0440243be0c93742e2e463b3104b
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2013-02-08 11:38:46 +1100

    work on source view

--- SourceView.hs
+++ SourceView.hs
@@ -31,19 +31,33 @@ type Trace = [TraceEntry]
 
 data SourceView c a = SourceView {
     svSpec         :: C.Spec,
-    svPID          :: I.PID,                     -- PID set in the process selection menu
-    svState        :: D.State a Store,           -- Current state set via view callback
-    svTrace        :: Trace,                     -- Steps from the current state
-    svTracePos     :: Int,                       -- Current position in the trace
+    svState        :: D.State a Store,            -- current state set via view callback
+
+    -- Trace
+    svTrace        :: Trace,                      -- steps from the current state
+    svTracePos     :: Int,                        -- current position in the trace
+    svTraceCombo   :: G.ComboBox,                 -- trace combo box
+    svTraceStore   :: G.ListStore Int,            -- indices in the trace that correspond to visible locations
+    svTraceUndo    :: G.ToolButton,               -- back button
+    svTraceRedo    :: G.ToolButton,               -- forward button
 
     -- Process selector
-    svProcessCombo :: G.ComboBox,                -- process selection combo box
-    svProcessStore :: G.TreeStore (I.PID, Bool), -- tree store that backs the process selector
+    svPID          :: I.PID,                      -- PID set in the process selection menu
+    svProcessCombo :: G.ComboBox,                 -- process selection combo box
+    svProcessStore :: G.TreeStore (I.PID, Bool),  -- tree store that backs the process selector
 
     -- Stack view
-    svStackView    :: G.TreeView,                -- stack view
-    svStackStore   :: G.ListStore Frame,         -- store containing list of stack frames
-    svStackFrame   :: Int                        -- selected stack frame (0 = top)
+    svStackView    :: G.TreeView,                 -- stack view
+    svStackStore   :: G.ListStore Frame,          -- store containing list of stack frames
+    svStackFrame   :: Int,                        -- selected stack frame (0 = top)
+
+    -- Watch
+    svWatchView    :: G.TreeView,
+    svWatchStore   :: G.ListStore (Maybe String), -- store containing watch expressions
+
+    -- Source window
+    svSourceView   :: G.SourceView,
+    svSourceTag    :: G.TextTag                  -- tag to mark current selection current selection
 }
 
 --------------------------------------------------------------
@@ -53,15 +67,20 @@ data SourceView c a = SourceView {
 sourceViewNew :: (D.Rel c v a s) => C.Spec -> D.RModel c a Store -> IO (D.View a Store)
 sourceViewNew spec model = do
     ref = newIORef $ SourceView { svSpec         = spec
-                                , svPID          = error "SourceView: PID undefined"
                                 , svState        = error "SourceView: state undefined"
                                 , svTrace        = []
                                 , svTracePos     = 0
+                                , svTraceCombo   = error "SourceView: svTraceCombo undefined"
+                                , svTraceStore   = error "SourceView: svTraceStore undefined"
+                                , svTraceUndo    = error "SourceView: svTraceUndo undefined"
+                                , svTraceRedo    = error "SourceView: svTraceRedo undefined"
+                                , svPID          = error "SourceView: PID undefined"
                                 , svProcessCombo = error "SourceView: svProcessCombo undefined"
                                 , svProcessStore = error "SourceView: svProcessStore undefined"
                                 , svStackView    = error "SourceView: svStackView undefined"
                                 , svStackStore   = error "SourceView: svStackStore undefined"
                                 , svStackFrame   = error "SourceView: svStackFrame undefined"
+                                , svWatchStore   = error "SourceView: svWatchStore undefined"
                                 }
 
     vbox <- G.vBoxNew False 0
@@ -184,7 +203,7 @@ run ref = do
 --------------------------------------------------------------
 
 -- Process selector --
-processSelectorCreate :: RSourceView c a  -> IO G.Widget
+processSelectorCreate :: RSourceView c a -> IO G.Widget
 processSelectorCreate ref = do
     sv <- readIORef ref
     combo <- G.comboBoxNew
@@ -267,23 +286,215 @@ stackViewDisable ref = do
     G.listStoreClear $ svStackStore sv
 
 -- Trace --
-traceViewCreate ref
-traceViewDisable ref
-traceViewUpdate ref
+traceViewCreate :: RSourceView c a -> IO G.Widget
+traceViewCreate ref = do
+    hbox <- G.hBoxNew False 0
+    G.widgetShow hbox
+
+    -- undo button
+    undo <- G.toolButtonNewFromStock G.stockUndo
+    G.widgetShow undo
+    G.boxPackStart hbox undo G.PackNatural 0
+    G.onToolButtonClicked undo (do modifyIORef ref (\sv -> traceSetPos sv (svTracePos sv - 1))
+                                   updateDisplays ref)
+
+    -- trace
+    combo <- G.comboBoxNew
+    G.widgetShow combo
+    store <- G.treeStoreNew []
+    rend <- G.cellRendererTextNew
+    G.cellLayoutPackStart combo rend True
+    G.cellLayoutSetAttributeFunc combo rend store $ 
+        (\iter -> path <- G.treeModelGetPath store iter
+                  idx  <- G.treeStoreGetValue store path
+                  sv   <- readIORef ref
+                  let txt = take 48 $ show $ replace "\n" " " text $
+                            case locAct $ getLocLabel sv idx of
+                                 ActStat s -> pack $ show s
+                                 ActExpr e -> pack $ show e
+                                 _         -> "?"
+                  set rend [cellTextMarkup := "<span weight=" ++ if idx == svTracePos sv then "bold" else "normal" ++ ">" ++ txt ++ "</span>"])
+    G.on combo G.changed (tracePosChanged ref)
+
+    -- redo button
+    redo <- G.toolButtonNewFromStock G.stockRedo
+    G.widgetShow redo
+    G.boxPackStart hbox redo G.PackNatural 0
+    G.onToolButtonClicked redo (do modifyIORef ref (\sv -> traceSetPos sv (svTracePos sv + 1))
+                                   updateDisplays ref)
+
+    modifyIORef ref $ (\sv -> sv { svTraceStore = store
+                                 , svTraceCombo = combo
+                                 , svTraceUndo  = undo
+                                 , svTraceRedo  = redo})
+
+    return $ G.toWidget hbox
+
+tracePosChanged :: RSourceView c a -> IO ()
+tracePosChanged ref = do
+    sv <- readIORef ref
+    miter <- comboBoxGetActiveIter $ svTraceCombo sv
+    when isJust miter $ 
+        do storeidx <- G.listStoreIterToIndex $ fromJust miter
+           pos <- G.listStoreGetValue (svTraceStore sv) storeidx
+           modifyIORef ref (\sv -> traceSetPos sv pos)
+           updateDisplays ref
+
+
+traceViewDisable :: RSourceView c a -> IO ()
+traceViewDisable ref = do
+    sv <- readIORef ref
+    G.listStoreClear $ svTraceStore sv
+    G.widgetSetSensitive (svTraceUndo sv) False
+    G.widgetSetSensitive (svTraceRedo sv) False
+    G.widgetSetSensitive (svTraceCombo sv) False
+
+traceViewUpdate :: RSourceView c a -> IO ()
+traceViewUpdate ref = do
+    sv <- readIORef ref
+    -- update store
+    G.listStoreClear $ svTraceStore sv
+    mapM (listStoreAppend (svTraceStore sv) . snd) 
+         $ filter (\(t,i) -> case locAct $ getLocLabel sv i
+                                  ActNone -> False
+                                  _       -> True)
+         $ zip (svTrace sv) [0..]
+
+    -- set selection
+    items <- G.listStoreToList (svTraceStore sv)
+    case findIndex (svTracePos sv ==) item of
+         Nothing -> G.comboBoxSetActive (svTraceCombo sv) (-1)
+         Just i  -> G.comboBoxSetActive (svTraceCombo sv) i
+
+    -- enable/disable buttons
+    G.widgetSetSensitive (svTraceUndo sv) (svTracePos sv /= 0)
+    G.widgetSetSensitive (svTraceRedo sv) (svTracePos sv /= length (svTrace sv) - 1)
+
+
+traceAppend :: SourceView c a -> Store -> I.Stack -> SourceView c a
+traceAppend sv store stack = sv {svTrace = tr, svTracePos = pos}
+    where tr  = take (svTracePos sv + 1) (svTrace sv) ++ [TraceEntry stack store]
+          pos = length tr - 1
+
+traceSetPos :: SourceView c a -> Int -> SourceView c a
+traceSetPos sv i | (i >= length $ svTrace sv) || (i < 0) = sv
+                 | otherwise = sv {svTracePos = i}
+
 
 -- Watch --
-watchCreate ref
-watchUpdate ref
-watchDisable ref
+
+watchCreate :: RSourceView c a -> IO G.Widget
+watchCreate ref = do
+    view <- G.treeViewNew
+    G.widgetShow view
+    G.boxPackStart vbox view G.PackGrow 0
+
+    store <- G.treeStoreNew [Nothing]
+    exprend <- G.cellRendererTextNew
+    G.cellLayoutPackStart view exprend True
+    G.cellLayoutSetAttributeFunc view exprend store $ 
+        (\iter -> idx  <- G.listStoreIterToIndex iter
+                  mexp <- G.listStoreGetValue store idx
+                  set exprend [ cellTextEditable := True
+                              , cellTextMarkup := case mexp of 
+                                                       Nothing  -> "<i>Add watch</i>"
+                                                       Just exp -> exp])
+    G.on exprend G.edited (\_ _ -> watchUpdate ref)
+
+    valrend <- G.cellRendererTextNew
+    G.cellLayoutPackStart view valrend True
+    G.cellLayoutSetAttributeFunc view valrend store $ 
+        (\iter -> idx  <- G.listStoreIterToIndex iter
+                  mexp <- G.listStoreGetValue store idx
+                  set valrend [cellTextMarkup := case mexp of 
+                                                      Nothing  -> ""
+                                                      Just exp -> case evalStrInScope exp of
+                                                                       Left e  -> e
+                                                                       Right v -> show v])
+
+    G.on view G.keyPressEvent (do key <- eventKeyVal
+                                  when (key == "Delete") $ liftIO $ watchDelete ref)
+    modifyIORef ref (\sv -> sv{svWatchView = view, svWatchStore = store})
+
+    G.treeViewSetModel view store
+    return $ G.toWidget view
+
+watchDelete :: RSourceView c a -> IO ()
+watchDelete ref = do
+    sv <- readIORef ref
+    (idx:_, _) <- G.treeViewGetCursor (svWatchView sv)
+    G.listStoreRemove (svWatchStore sv) idx
+    watchUpdate ref
+
+watchUpdate :: RSourceView c a -> IO ()
+watchUpdate ref = do
+    store <- getIORef svWatchStore ref
+    items <- G.listStoreToList store
+    -- make sure that there is an empty slot in the end
+    let items' = (filter isJust items) ++ [Nothing]
+    -- refill the list to force watch update
+    G.listStoreClear store
+    mapM (G.listStoreAppend store) items'
+    return ()
+
+watchDisable :: RSourceView c a -> IO ()
+watchDisable _ = return ()
 
 
 -- Source --
-sourceWindowUpdate ref
-sourceWindowDisable ref
+sourceWindowCreate :: RSourceView c a -> IO G.Widget
+sourceWindowCreate ref = do
+    view <- G.textViewNew
+    G.widgetShow view
+    tag <- G.textTagNew
+    G.set tag [textTagBackground := "grey"]
+    modifyIORef ref (\sv -> sv {svSourceView = view, svSourceTag = tag})
+    return $ G.toWidget view
+
+
+sourceWindowUpdate :: RSourceView c a -> IO ()
+sourceWindowUpdate ref = do
+    sv <- readIORef ref
+    let cfa = cfaAtFrame sv (svStackFrame sv)
+        loc = fLoc $ (currentStack sv) !! (svStackFrame sv)
+        p = case locAct $ I.cfaLocLabel cfa loc of
+                 ActNone   -> return ()
+                 ActExpr e -> sourceSetPos sv $ pos e
+                 ActStat s -> sourceSetPos sv $ pos s
+
+sourceWindowDisable :: RSourceView c a -> IO ()
+sourceWindowDisable _ = return ()
+
+sourceSetPos :: SourceView c a -> Pos -> IO ()
+sourceSetPos sv (from, to) = do
+    let fname = sourceName from
+    do src <- readFile fname
+       buf <- G.textBufferNew Nothing
+       G.textBufferSetText buf src
+       G.textViewSetBuffer (svSourceView sv) buf
+       istart <- G.textBufferGetStartIter buf
+       iend <- G.textBufferGetEndIter buf
+       ifrom <- G.textBufferGetIterAtLineOffset buf (sourceLine from) (sourceColumn from)
+       ito <- G.textBufferGetIterAtLineOffset buf (sourceLine to) (sourceColumn to)
+       G.textBufferRemoveTag buf (svSourceTag sv) istart iend
+       G.textBufferApplyTag buf (svSourceTag sv) ifrom ito
+       G.textViewScrollToIter (svSourceView sv) ifrom 0.5 Nothing
+    `catch` (\e -> return ())
+
+
 
 -- Command buttons --
-
-commandButtonsUpdate ref
+commandButtonsUpdate :: RSourceView c a -> IO ()
+commandButtonsUpdate ref = do
+    lab <- getIORef currentLocLabel ref
+    -- enable step and run buttons if we are not at a pause location or
+    -- if we are in a pause location and the wait condition is true
+    let en = case lab of
+                  LInst _      -> True
+                  LPause _ _ c -> eval c == BoolVal True 
+                  LFinal _ _   -> True
+    G.widgetSetSensitive (svStepButton sv) en
+    G.widgetSetSensitive (svRunButton sv) en
 
 commandButtonsDisable ref
     -- disable command buttons
@@ -298,16 +509,19 @@ commandButtonsDisable ref
 -- Given a snapshot of the store at a pause location, compute
 -- process stack.
 stackFromStore :: Store -> I.PID -> Stack
-stackFromStore s pid = 
-    -- concatenate stacks from each process down the branch of the process tree
-    concat $ reverse $ map (\pid -> stackFromStore' s pid Nothing) $ tail $ inits pid
+stackFromStore s pid = fst $ unzip $ stackFromStore' s pid Nothing
 
-stackFromStore' :: Store -> I.PID -> Maybe String -> Stack
+--    -- concatenate stacks from each process down the branch of the process tree
+--    concat $ reverse $ map (\pid -> stackFromStore' s pid Nothing) $ tail $ inits pid
+
+-- Returns extended version of the stack with PID and task name 
+-- attached to each frame
+stackFromStore' :: Store -> I.PID -> Maybe String -> [(Frame, (PID, Maybe String))]
 stackFromStore' s pid methname = stack' ++ stack
     where cfa    = C.getCFA pid methname
           loc    = storeGetLoc store pid methname
           label  = cfaLocLabel loc cfa
-          stack  = locStack label
+          stack  = zip (locStack label) (repeat (pid, methname))
           -- If this location corresponds to a task call, recurse into task's CFA
           stack' = case label of 
                         LPause _ _ e -> case isWaitForTask e of
@@ -317,6 +531,13 @@ stackFromStore' s pid methname = stack' ++ stack
 
 
 isProcEnabled :: SourceView c a -> I.PID -> Bool
+isProcEnabled sv pid = 
+    where (frame, (pid, mmeth)) = head $ stackFromStore' (currentStore sv) pid Nothing
+          cfa = C.getCFA pid mmeth
+          case cfaLocLabel (fLoc frame) cfa of
+               LPause _ _ cond -> eval cond == BoolVal True
+               _               -> True
+
 
 -- update all displays
 updateDisplays :: RSourceView c a -> IO ()
@@ -351,6 +572,8 @@ disable ref = do
     watchDisable ref
 
 
+-- Access current location in the trace --
+
 currentCFA :: SourceView -> I.CFA
 
 currentLoc :: SourceView -> I.Loc
@@ -360,6 +583,22 @@ currentLocLabel :: SourceView -> I.LocLabel
 currentStore :: SourceView -> Store
 
 currentStack :: SourceView -> I.Stack
+
+cfaAtFrame :: SourcView -> Int -> I.CFA
+cfaAtFrame sv frame
+    --
+
+-- Access arbitrary location in the trace 
+getCFA :: SourceView -> Int -> I.CFA
+
+getLoc :: SourceView -> Int -> I.Loc
+
+getLocLabel :: SourceView -> Int -> I.LocLabel
+
+getStore :: SourceView -> Int -> Store
+
+getStack :: SourceView -> Int -> I.Stack
+
 
 -- Execute one CFA transition
 -- Returns True if the step was performed successfully and
@@ -373,7 +612,7 @@ microstep ref = do
     sv' <- foldM resolveTran sv transitions
     case mapMaybe (microstep' sv') transitions of
          []                      -> return False
-         (Just (store, stack)):_ -> do writeIORef ref sv' $ traceAppend sv store stack
+         (Just (store, stack)):_ -> do writeIORef ref $ traceAppend sv' store stack
                                        return True
 
 microstep' :: SourceView c a -> (I.Loc, I.TranLabel) -> Maybe (Store, I.Stack)
